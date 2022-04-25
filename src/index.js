e60279779659a29c590df10367c42ac9a9660992
lovelin0523 <18356201582@163.com>
2022-04-25 19:00:52 +0800

    新增platform模块

--- src/index.js
+++ src/index.js
@@ -7,6 +7,25 @@ const date = require('./date')
 const color = require('./color')
 const file = require('./file')
 const string = require('./string')
+const platform = require('./platform')
+
+const packages = require('../package.json')
+
+console.log(
+    '%c感谢使用' +
+        packages.name +
+        '，当前版本：%c v' +
+        packages.version +
+        '\n%c如果你觉得' +
+        packages.name +
+        '还不错，不妨去github点个star\ngithub地址：%c' +
+        packages.repository.url,
+    'color:#808080;',
+    'color:#008a00',
+    'color:#808080;',
+    'color:#008a00'
+)
+
 module.exports = {
     number,
     data,
@@ -16,5 +35,6 @@ module.exports = {
     date,
     color,
     file,
-    string
+    string,
+    platform
 }
